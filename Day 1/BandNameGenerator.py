6c007ae08351aa32dc4985e7d2a02103ca41b220
amul patle <amulpatle3@gmail.com>
2023-09-18 12:18:44 +0530

    Day-1

---
+++ Day 1/BandNameGenerator.py
@@ -0,0 +1,7 @@
+print("Welcome to band Name Generator")
+
+cityName = input("What is name of the city you grew up in ?")
+
+patName = input("what's your pat's name?")
+
+print("Your band name could be "+ cityName + " " + patName)
