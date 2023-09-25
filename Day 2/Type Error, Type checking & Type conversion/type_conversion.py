153e6ea87adf668e3b82baf42aac399adf8f2333
amul patle <amulpatle3@gmail.com>
2023-09-25 19:52:31 +0530

    modified

--- Day 2/Type Error, Type checking & Type conversion/type_conversion.py
+++ Day 2/Type Error, Type checking & Type conversion/type_conversion.py
@@ -2,4 +2,6 @@ nam_char = len(input("Enter your Name"))
 
 new_num_char = str(nam_char)
 
+# we can only concatinate string to string and int to int 
+# we can not concatinate string to int and int to string
 print("your Name have " + new_num_char + " charater")
