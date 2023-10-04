61c0578e97b50f6a414526e1e2aeab01d77456f6
amul patle <amulpatle3@gmail.com>
2023-10-04 19:09:21 +0530

    add flag to check letter

--- Day 7/temp3/temp3.py
+++ Day 7/temp3/temp3.py
@@ -15,12 +15,18 @@ print(display)
 # letter = input("Guess a Letter ").lower()
 
 for j in range(len(choosen_word)):
+    flag = False
     letter = input("Guess a Letter ").lower()
     for i in range(len(choosen_word)):
         if letter == choosen_word[i]:
             display[i] = letter
-    if letter == choosen_word[i]:
-        print(display)
-    else:
-        print("lost a life")
+            flag = True
+            print(display)
+    if flag == False:
+        print("Incorrect")
+
+    # if letter == choosen_word[i]:
+    #     print(display)
+    # else:
+    #     print("lost a life")
 
