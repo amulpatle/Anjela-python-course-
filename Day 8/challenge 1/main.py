a9723a0e4a7bb042e7c7cffdd32c4e5b91e5d4a2
amul patle <amulpatle3@gmail.com>
2023-10-05 13:52:11 +0530

    shift letter

---
+++ Day 8/challenge 1/main.py
@@ -0,0 +1,24 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+
+def greet():
+    print("Good Morning !!")
+    print("how do you do")
+    print("Is't the weather nice")
+
+greet()
+
+def greet_with_name(name):
+    print(f"Good Morning , {name}")
+    print(f"how do you do {name}")
+
+greet_with_name("Amul")
+
+def greet_with(name,location):
+    print(f"hello {name}")
+    print(f"I live in {location}")
+
+greet_with("amul","balaghat")
