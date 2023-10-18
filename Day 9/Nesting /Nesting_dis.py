0d86d0113a32f65346fc9713004f3c9a6eb4cb32
amul patle <amulpatle3@gmail.com>
2023-10-18 09:57:41 +0530

    nesting in dicstionary

---
+++ Day 9/Nesting /Nesting_dis.py
@@ -0,0 +1,20 @@
+traveling_log = {"mp":["balaghat","Bhopal, Indore"]
+                 ,"Gujrat":{"not_visited" :["surat","patna"],"total_visited":12}}
+
+# for cities in traveling_log:
+#     print(traveling_log[cities])
+
+
+planning_to_travel = [
+
+    {"mp":["ujjain","Indore","omkaleshwar","jabalpur"],
+     "total_number_visited":12
+    },
+    {
+        "tamilnadu":["tiruannatpuram","kanyaKumari"],
+        "total_number_visited":5
+    }
+]
+
+for i in  planning_to_travel:
+    print(i)
