e261e1bf7088f192f137e8f2b2730298095941f8
amul patle <amulpatle3@gmail.com>
2023-12-17 01:56:00 +0530

    draw dotted line

--- Day 18/Main.py
+++ Day 18/Main.py
@@ -4,16 +4,22 @@ timmy = Turtle()
 timmy.shape("turtle")
 
 
-timmy.forward(100)
-timmy.left(90)
-timmy.forward(100)
-timmy.left(90)
-timmy.forward(100)
-timmy.left(90)
-timmy.forward(100)
-timmy.left(90)
-
-
-
+# timmy.forward(100)
+# timmy.left(90)
+# timmy.forward(100)
+# timmy.left(90)
+# timmy.forward(100)
+# timmy.left(90)
+# timmy.forward(100)
+# timmy.left(90)
+# screen = Screen()
+# screen.exitonclick()
+for _ in range (20):
+    timmy.forward(10)
+    # timmy.delay(50)     
+    timmy.pd()
+    timmy.forward(10)
+    timmy.penup()
+    
 screen = Screen()
 screen.exitonclick()
