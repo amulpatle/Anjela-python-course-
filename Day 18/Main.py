94cfde0892cbe0c8eca4fb630761ff71b400620c
amul patle <amulpatle3@gmail.com>
2023-12-17 01:47:19 +0530

    made a square

---
+++ Day 18/Main.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+
+
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+
+
+
+screen = Screen()
+screen.exitonclick()
