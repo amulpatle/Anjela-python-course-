676e67f41294784286839b3f2cd51b85f3a7e966
amul patle <amulpatle3@gmail.com>
2023-12-16 23:27:17 +0530

    compilte quiz project

---
+++ Day 17/Quiz Project/data.py
@@ -0,0 +1,121 @@
+# question_data = [
+# {"text": "A slug's blood is green.", "answer": "True"},
+# {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+# {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+# {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+# {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
+# {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
+# {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+# {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+# {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+# {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+# {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+# {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+# ]
+
+question_data = [
+
+
+      {
+         "type":"boolean",
+         "difficulty":"medium",
+         "category":"Geography",
+         "question":"\"Mongolia\" was a part of the now non-existent U.S.S.R.",
+         "correct_answer":"False",
+         "incorrect_answers":[
+            "True"
+         ]
+      },
+      {
+         "type":"boolean",
+         "difficulty":"medium",
+         "category":"Science: Computers",
+         "question":"All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
+         "correct_answer":"False",
+         "incorrect_answers":[
+            "True"
+         ]
+      },
+      {
+         "type":"boolean",
+         "difficulty":"medium",
+         "category":"Entertainment: Video Games",
+         "question":"DragonForce's 'Through the Fire and Flames' is widely considered to be the hardest song in the Guitar Hero series.",
+         "correct_answer":"True",
+         "incorrect_answers":[
+            "False"
+         ]
+      },
+      {
+         "type":"boolean",
+         "difficulty":"medium",
+         "category":"Vehicles",
+         "question":"The Chevrolet Corvette has always been made exclusively with V8 engines only.",
+         "correct_answer":"False",
+         "incorrect_answers":[
+            "True"
+         ]
+      },
+      {
+         "type":"boolean",
+         "difficulty":"medium",
+         "category":"Entertainment: Television",
+         "question":"In \"Star Trek\", Klingons respect William Shakespeare, they even suspect him having a Klingon lineage.",
+         "correct_answer":"True",
+         "incorrect_answers":[
+            "False"
+         ]
+      },
+      {
+         "type":"boolean",
+         "difficulty":"easy",
+         "category":"Entertainment: Film",
+         "question":"\"Foodfight!\" earned less than $80,000 at the box office.",
+         "correct_answer":"True",
+         "incorrect_answers":[
+            "False"
+         ]
+      },
+      {
+         "type":"boolean",
+         "difficulty":"easy",
+         "category":"Entertainment: Japanese Anime & Manga",
+         "question":"Clefairy was intended to be Ash's starting Pokémon in the pilot episode of the cartoon.",
+         "correct_answer":"True",
+         "incorrect_answers":[
+            "False"
+         ]
+      },
+      {
+         "type":"boolean",
+         "difficulty":"easy",
+         "category":"Entertainment: Film",
+         "question":"The word \"Inception\" came from the 2010 blockbuster hit \"Inception\".",
+         "correct_answer":"False",
+         "incorrect_answers":[
+            "True"
+         ]
+      },
+      {
+         "type":"boolean",
+         "difficulty":"easy",
+         "category":"General Knowledge",
+         "question":"Dihydrogen Monoxide was banned due to health risks after being discovered in 1983 inside swimming pools and drinking water.",
+         "correct_answer":"False",
+         "incorrect_answers":[
+            "True"
+         ]
+      },
+      {
+         "type":"boolean",
+         "difficulty":"medium",
+         "category":"History",
+         "question":"Martin Luther King Jr. and Anne Frank were born the same year.",
+         "correct_answer":"True",
+         "incorrect_answers":[
+            "False"
+         ]
+      }
+   ]
+
+# here we use https://opentdb.com/ website to generate api base questions
