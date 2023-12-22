dbbb3b21c6dbd1e6ecabc505a985af3b144776b1
amul patle <amulpatle3@gmail.com>
2023-12-23 00:02:58 +0530

    move snake & add animation

--- Day 20/Snake game/main.py
+++ Day 20/Snake game/main.py
@@ -1,19 +1,35 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
 segment_position = [(0,0),(-20,0),(-40,0)]
 
 segment1 = Turtle(shape="square")
 segment1.color("white")
 
+is_game_on = True
+
+segment = []
+
 for position in segment_position:
     segment1 = Turtle(shape="square")
     segment1.color("white")
+    segment1.penup()
     segment1.goto(position)
+    segment.append(segment1)
+
+# screen.update()
+
+while is_game_on:
+    screen.update()
+    for seg in segment:
+        seg.forward(10)
+        time.sleep(0.3)
 
 screen.exitonclick()
