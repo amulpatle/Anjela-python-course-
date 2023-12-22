e6d02b9b559f67e90f667aa2be55122787bf9ddf
amul patle <amulpatle3@gmail.com>
2023-12-22 22:51:48 +0530

    set snake position

--- Day 20/Snake game/main.py
+++ Day 20/Snake game/main.py
@@ -4,5 +4,16 @@ screen = Screen()
 
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
+screen.title("My Snake Game")
+
+segment_position = [(0,0),(-20,0),(-40,0)]
+
+segment1 = Turtle(shape="square")
+segment1.color("white")
+
+for position in segment_position:
+    segment1 = Turtle(shape="square")
+    segment1.color("white")
+    segment1.goto(position)
 
 screen.exitonclick()
