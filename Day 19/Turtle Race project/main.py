af8f0144d8fd331510518a1bb2f64b99af85dca8
amul patle <amulpatle3@gmail.com>
2023-12-22 11:49:21 +0530

    make 6 turtle

--- Day 19/Turtle Race project/main.py
+++ Day 19/Turtle Race project/main.py
@@ -1,15 +1,20 @@
 from turtle import Turtle, Screen
 
-tim = Turtle(shape="turtle")
 screen = Screen()
 
 
 screen.setup(width=500,height=400)
-
+colors = ["blue","orange","yellow","green","red","purple",]
+y_positon = [-70,-40,-10,20,50,80]
 user_bet = screen.textinput(title="Make a bet", prompt="which turtle will won the match? Make a bet")
 print(user_bet)
 
-tim.penup()
-tim.goto(x=-230,y = -100)
+
+
+for ind in range(0,6):
+    tim = Turtle(shape="turtle")
+    tim.color(colors[ind])
+    tim.penup()
+    tim.goto(x=-230,y = y_positon[ind])
 
 screen.exitonclick()
