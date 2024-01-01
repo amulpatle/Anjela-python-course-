c9d7ad0317ca91dc204f5d7900fd31490854befb
amul patle <amulpatle3@gmail.com>
2024-01-01 14:21:31 +0530

    make scorecard & corner condition

--- Day 20/Snake game/main.py
+++ Day 20/Snake game/main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -21,6 +22,7 @@ screen.onkey(snake.down,"Down")
 screen.onkey(snake.left,"Left")
 screen.onkey(snake.right,"Right")
 
+scoreboard = Scoreboard()
 
 while is_game_on:
     screen.update()
@@ -31,6 +33,14 @@ while is_game_on:
     if snake.head.distance(food)  < 15:
         # print("yammy")
         food.refresh()
+        snake.extend()
+        Scoreboard.increase_score()
+        
+    # detect collition with walls
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280 :
+        is_game_on = False
+        scoreboard.game_over()
+        
     
 
 screen.exitonclick()
