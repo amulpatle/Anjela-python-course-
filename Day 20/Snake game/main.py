669ee77ae6a9fc0e32a4fd710096f647d258a48a
amul patle <amulpatle3@gmail.com>
2024-01-01 09:43:43 +0530

    detect collition

--- Day 20/Snake game/main.py
+++ Day 20/Snake game/main.py
@@ -24,9 +24,12 @@ screen.onkey(snake.right,"Right")
 
 while is_game_on:
     screen.update()
-    time.sleep(0.1)
-    
+    time.sleep(0.1) 
     snake.move()
+    
+    # detect snake collition with food
+    if snake.head.distance(food)  < 15:
+        print("yammy")
     
 
 screen.exitonclick()
