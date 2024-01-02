084c37eecbd94284739ea27c04d4d53a55ed74b2
amul patle <amulpatle3@gmail.com>
2024-01-02 11:33:56 +0530

    bouce ball with both paddles

--- Day 21/Pong Game/Main.py
+++ Day 21/Pong Game/Main.py
@@ -30,6 +30,9 @@ while is_game_on:
     screen.update()
     ball.move()
     if ball.ycor() > 280 or ball.ycor() < -280 :
-        ball.bounce()
+        ball.bounce_y()
+        
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 screen.exitonclick()
