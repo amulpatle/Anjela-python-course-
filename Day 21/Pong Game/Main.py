4ca9866f9174013bfda4c274fbb3c0810b583a91
amul patle <amulpatle3@gmail.com>
2024-01-02 09:36:25 +0530

    design paddle

--- Day 21/Pong Game/Main.py
+++ Day 21/Pong Game/Main.py
@@ -1,9 +1,19 @@
 from turtle import Screen
+from turtle import Turtle
 
 
 screen = Screen()
+paddle = Turtle()
+
 screen.bgcolor("black")
 screen.setup(width=800,height=600)
 screen.title("pong")
+
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5,stretch_len=1)
+paddle.penup()
+paddle.goto(350,0)
+
 
 screen.exitonclick()
