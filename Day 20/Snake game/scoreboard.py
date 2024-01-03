ba5c3f409d5627e881cd894dcd1b318be2065d8e
amul patle <amulpatle3@gmail.com>
2024-01-03 16:42:41 +0530

    store prev score in storage

--- Day 20/Snake game/scoreboard.py
+++ Day 20/Snake game/scoreboard.py
@@ -8,7 +8,9 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("/home/amul/Documents/anjela python/Day 20/Snake game/data.txt",'r')as data:
+            self.high_score = int(data.read())
+        # self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -26,6 +28,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score  > self.high_score:
             self.high_score = self.score
+            with open("/home/amul/Documents/anjela python/Day 20/Snake game/data.txt",'w') as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
