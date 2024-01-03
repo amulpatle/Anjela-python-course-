6cae2485681634db377eb077ef4e8cf1d27289bd
amul patle <amulpatle3@gmail.com>
2024-01-03 12:09:12 +0530

    complite

--- Day 23/main.py
+++ Day 23/main.py
@@ -28,6 +28,7 @@ while game_is_on:
     # detect collition with cars
     for car in car_manager.all_cars:
         if car.distance(player) < 20 :
+            scoreboard.game_over()
             game_is_on = False
     
     # Detect succeful crossing
