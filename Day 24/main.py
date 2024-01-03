b3a49b5834cd2d5a880e69cdc0669cd4c01ad422
amul patle <amulpatle3@gmail.com>
2024-01-03 15:50:28 +0530

    with keyword open file

--- Day 24/main.py
+++ Day 24/main.py
@@ -1,4 +1,6 @@
-file = open("/home/amul/Documents/anjela python/Day 24/my_file.txt")
-contents = file.read()
-print(contents)
-file.close()
+
+#  if we write this way we don't need to close file menualy
+with  open("/home/amul/Documents/anjela python/Day 24/my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+    # file.close()
