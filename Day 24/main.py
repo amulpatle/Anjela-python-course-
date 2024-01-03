96a9499fc768ee952632ffc4d90d0ca45fc0a8b9
amul patle <amulpatle3@gmail.com>
2024-01-03 15:53:16 +0530

    open file in write mode

--- Day 24/main.py
+++ Day 24/main.py
@@ -1,6 +1,14 @@
 
 #  if we write this way we don't need to close file menualy
-with  open("/home/amul/Documents/anjela python/Day 24/my_file.txt") as file:
-    contents = file.read()
-    print(contents)
+# with  open("/home/amul/Documents/anjela python/Day 24/my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
     # file.close()
+    
+# file = open("/home/amul/Documents/anjela python/Day 24/my_file.txt") 
+# contents = file.read()
+# print(contents)
+# file.close()
+
+with open("/home/amul/Documents/anjela python/Day 24/my_file.txt","w") as file:
+    file.write("hey,this is new one")
