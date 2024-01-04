328e374fb2bad9ba909d9106fc346d59230ccc47
amul patle <amulpatle3@gmail.com>
2024-01-04 09:13:59 +0530

    add image to screen

---
+++ Day 25/U.S. State Game/main.py
@@ -0,0 +1,8 @@
+import turtle 
+
+screen = turtle.Screen()
+screen.title("U.S. State Game")
+image = "/home/amul/Documents/anjela python/Day 25/U.S. State Game/blank_states_img.gif"
+screen.addshape(image)
+
+screen.exitonclick()
