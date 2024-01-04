ad167b0bd2afc69bf284bde59431befbe10493b0
amul patle <amulpatle3@gmail.com>
2024-01-04 11:46:49 +0530

    take input from user

--- Day 25/U.S. State Game/main.py
+++ Day 25/U.S. State Game/main.py
@@ -3,6 +3,12 @@ import turtle
 screen = turtle.Screen()
 screen.title("U.S. State Game")
 image = "/home/amul/Documents/anjela python/Day 25/U.S. State Game/blank_states_img.gif"
-screen.addshape(image)
+screen.bgpic(image)
 
-screen.exitonclick()
+answer_state = screen.textinput(title="Guess the State", prompt="What is another state name")
+
+def get_mouse_click_coor(x,y):
+    print(x,y)
+
+turtle.onscreenclick(get_mouse_click_coor)
+turtle.mainloop()
