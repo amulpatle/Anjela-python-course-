537224ce3036d9e456fd62d6e04acbf0b86630aa
amul patle <amulpatle3@gmail.com>
2024-01-04 08:36:44 +0530

    working with pandas module

--- Day 25/main.py
+++ Day 25/main.py
@@ -14,4 +14,18 @@
 import pandas
 
 data = pandas.read_csv("/home/amul/Documents/anjela python/Day 25/weather_data.csv")
-print(data)       
+# print(data) 
+
+# data_list = data["temp"].to_list()
+# print(data_list) 
+# avg_temp = sum(data_list)/len(data_list)
+# print(avg_temp)
+     
+print(data[data.day == "Monday"])
+
+print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+monday_temp = int(monday.temp)
+monday_temp_f = monday_temp * 9/ 5 + 32
+print(monday_temp_f)
