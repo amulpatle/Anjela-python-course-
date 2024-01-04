42bb605d1a9eef264d8f9406ba3430541df006cb
amul patle <amulpatle3@gmail.com>
2024-01-04 07:42:06 +0530

    download pandas

--- Day 25/main.py
+++ Day 25/main.py
@@ -2,14 +2,16 @@
 #     data = data_file.readlines()
 #     print(data)
 
-import csv
+# import csv
+# with open("/home/amul/Documents/anjela python/Day 25/weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temprature = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temprature.append(int(row[1]))     
+#     print(temprature)
 
-with open("/home/amul/Documents/anjela python/Day 25/weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temprature = []
-    for row in data:
-        if row[1] != "temp":
-            temprature.append(int(row[1]))
-        
-    print(temprature)
-        
+import pandas
+
+data = pandas.read_csv("/home/amul/Documents/anjela python/Day 25/weather_data.csv")
+print(data)       
