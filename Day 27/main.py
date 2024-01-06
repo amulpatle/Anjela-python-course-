8acc611ea57db9ce72eeb384b76ea7f66c8ed3e5
amul patle <amulpatle3@gmail.com>
2024-01-06 12:33:56 +0530

    creating a lebel

--- Day 27/main.py
+++ Day 27/main.py
@@ -4,4 +4,8 @@ window = tkinter.Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=300)
 
+# Label
+
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack()
 window.mainloop()
