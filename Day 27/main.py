bfcfc6f0c67e1141fbcc4e19dc1be4579ce3c03a
amul patle <amulpatle3@gmail.com>
2024-01-06 15:29:43 +0530

    change lebel after click button

--- Day 27/main.py
+++ Day 27/main.py
@@ -1,11 +1,21 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=300)
 
 # Label
 
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+
+
+my_label = Label(text="i am label", font=("Arial", 24, "bold"))
 my_label.pack()
+
+def button_clicked():
+    print("I Got Clicked")
+    my_label.config(text = "Button got clicked")
+
+button = Button(text="click", command=button_clicked)
+button.pack()
+
 window.mainloop()
