52b7c858cb645e6a156477ac016c720a61d03d15
amul patle <amulpatle3@gmail.com>
2024-01-06 15:02:24 +0530

    **kwargs

--- Day 27/test.py
+++ Day 27/test.py
@@ -5,3 +5,19 @@ def add1(*args):
     return sum
 
 print(add1(2,3,4))
+
+# kwargs = key value pair 
+# it convert argument into dictionary key value
+
+def calculate(n,**kwargs):
+    # print(kwargs)
+    # print(type(kwargs))
+    for (key, value) in kwargs.items():
+        print(key,value)
+    n += kwargs["add"]
+    n *= kwargs["mult"]
+    print(n)
+    
+    
+
+calculate(2,add=3,mult = 5)#we can pass agrgument directly or by user defined name
