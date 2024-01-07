9790606409ddcdfe3740cf8ff9a8c1763303ba26
amul patle <amulpatle3@gmail.com>
2024-01-07 17:43:30 +0530

    set all label

--- Day 28/main.py
+++ Day 28/main.py
@@ -21,10 +21,24 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100,pady=50,bg = YELLOW)
 
+timer_label = Label(text="Timer",fg=GREEN,bg=YELLOW, font=(FONT_NAME,33,"bold"))
+# timer_label.size(50,20)
+timer_label.grid(column=1,row=0)
+
 canvas = Canvas(width=200,height=224,bg = YELLOW,highlightthickness=0)
 tomato_img = PhotoImage(file="/home/amul/Documents/anjela python/Day 28/tomato.png")
 canvas.create_image(100,112,image = tomato_img)
 canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
-canvas.pack()
+canvas.grid(row=1,column=1)
+
+
+start_button = Button(text="start",highlightthickness=0)
+start_button.grid(column=0,row=2)
+
+reset_button = Button(text="reset", highlightthickness=0)
+reset_button.grid(column=2,row=2)
+
+check_mark = Label(text="✔️", fg=GREEN,bg = YELLOW, )
+check_mark.grid(column=1,row=3)
 
 window.mainloop()
