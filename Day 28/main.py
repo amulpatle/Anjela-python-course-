c5e7924926db15db3e8ab01515d946c1d867cbcb
amul patle <amulpatle3@gmail.com>
2024-01-07 23:54:15 +0530

    change fg color

--- Day 28/main.py
+++ Day 28/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,19 +9,50 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5)
+    global reps
+    
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN*60
+    long_break_sec = LONG_BREAK_MIN*60
+    
+    #if it's the 1st/3rd/5th/7th rep:
+    # count_down(work_sec)
+    
+    #if it's the 8th rep:
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text="Break",fg=RED)
+    elif reps % 2== 0: 
+    #if it's 2nd/4th/6th rep:
+        count_down(short_break_sec)
+        timer_label.config(text="Break",fg=PINK)
+    else:
+        count_down(work_sec)
+        timer_label.config(text="work",fg=GREEN)
+    
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 def count_down(count):
-    canvas.itemconfigure(timer_text,text = count)
+    
+    count_min = math.floor(count/60)
+    count_sec = count%60
+    
+    if count_sec < 10 :
+        count_sec = f"0{count_sec}"
+    
+    canvas.itemconfigure(timer_text,text = f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000,count_down,count-1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 
