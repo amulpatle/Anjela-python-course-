22755337da433167c8c271b42ef318946b43dc36
amul patle <amulpatle3@gmail.com>
2024-01-08 18:01:40 +0530

    save password & email

--- Day 29/Password Manager/main.py
+++ Day 29/Password Manager/main.py
@@ -5,6 +5,17 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("/home/amul/Documents/anjela python/Day 29/Password Manager/data.txt",'a') as data_file:
+        data_file.write(f"{website} | {email} | {password}")
+        website_entry.delete(0,END)
+        password_entry.delete(0,END)
+        
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("password manager")
@@ -33,7 +44,7 @@ generatepassword_lable.grid(column=2,row=3)
 
 # buttons
 
-add_button = Button(text="Add",width=36)
+add_button = Button(text="Add",width=36, command=save)
 add_button.grid(column=1,row=4,columnspan=2)
 
 generate_password_button = Button(text="Generate Password")
@@ -43,9 +54,11 @@ generate_password_button.grid(row=3,column=2)
 
 website_entry = Entry(width=35)
 website_entry.grid(row = 1,column=1,columnspan=2)
+website_entry.focus()
 
 email_entry = Entry(width=35)
 email_entry.grid(row=2,column=1,columnspan=2)
+email_entry.insert(0,"xyz@gmail.com")
 
 password_entry = Entry(width=21)
 password_entry.grid(row=3,column=1)
