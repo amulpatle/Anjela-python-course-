80cba1c9ec0439266271158d7ad8b138be1b69e0
amul patle <amulpatle3@gmail.com>
2024-01-12 23:11:46 +0530

    set size

--- Day 31/Flash-card-project/main.py
+++ Day 31/Flash-card-project/main.py
@@ -1,2 +1,17 @@
 BACKGROUND_COLOR = "#B1DDC6"
+from tkinter import *
+window = Tk()
+window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
+window.title("flashy")
 
+canvas = Canvas(width=800,height=526)
+
+card_front_img = PhotoImage(file = "/home/amul/Documents/anjela python/Day 31/Flash-card-project/images/card_front.png")
+canvas.create_image(400,263,image = card_front_img)
+canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
+canvas.grid(row=0,column=0)
+
+
+
+
+window.mainloop()
