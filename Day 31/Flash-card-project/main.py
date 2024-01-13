20b03a77fbefcfc1dfd5f921615e539759177eb3
amul patle <amulpatle3@gmail.com>
2024-01-13 12:48:59 +0530

    create cross Button

--- Day 31/Flash-card-project/main.py
+++ Day 31/Flash-card-project/main.py
@@ -13,7 +13,8 @@ canvas.create_text(400,263,text="word",font=("Ariel",60,"bold"))
 canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
 canvas.grid(row=0,column=0)
 
-
-
+cross_image = PhotoImage(file="/home/amul/Documents/anjela python/Day 31/Flash-card-project/images/wrong.png")
+unknown_button = Button(image=cross_image)
+unknown_button.grid(row=1,column=0)
 
 window.mainloop()
