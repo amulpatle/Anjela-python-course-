625b2c3a6cc6815015024d08733f14e0bc5b4553
amul patle <amulpatle3@gmail.com>
2024-01-17 13:46:07 +0530

    complite

--- Day 33/kanye-quotes/main.py
+++ Day 33/kanye-quotes/main.py
@@ -1,9 +1,16 @@
 from tkinter import *
+import requests
 
 
 def get_quote():
     pass
     #Write your code here.
+    response = requests.get(url="https://api.kanye.rest/")
+    data = response.json()
+    txt = data["quote"]
+    canvas.itemconfig(quote_text,text = txt)
+    
+
 
 
 
@@ -12,12 +19,12 @@ window.title("Kanye Says...")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
+background_img = PhotoImage(file="/home/amul/Documents/anjela python/Day 33/kanye-quotes/background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+quote_text = canvas.create_text(150, 207, text="Kanya quote goes here", width=250, font=("Arial", 30, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
-kanye_img = PhotoImage(file="kanye.png")
+kanye_img = PhotoImage(file="/home/amul/Documents/anjela python/Day 33/kanye-quotes/kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
 
