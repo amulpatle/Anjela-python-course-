7e54ac58c52774f5deab485507e936fdabda6bb9
amul patle <amulpatle3@gmail.com>
2024-01-17 12:26:43 +0530

    get position

--- Day 33/main.py
+++ Day 33/main.py
@@ -5,4 +5,8 @@ response = requests.get(url="http://api.open-notify.org/iss-now.json")
 print(response.status_code)
 
 data = response.json()
-print(data)
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+iss_position = (longitude,latitude)
+# print(data)
+print(iss_position)
