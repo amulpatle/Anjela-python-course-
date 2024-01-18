4d74a595393d2f1ed251bc07809a5a8b4efa0c53
amul patle <amulpatle3@gmail.com>
2024-01-18 22:50:54 +0530

    call for qestions

--- Day 34/Quiz app/ui.py
+++ Day 34/Quiz app/ui.py
@@ -1,9 +1,11 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class UserInterface:
-    def __init__(self):
+    def __init__(self,quiz_brain:QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("quizila")
         # self.canvas = Canvas()
@@ -25,3 +27,7 @@ class UserInterface:
         self.false_button.grid(row=2,column=1)
         
         self.window.mainloop()
+    
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text,text = q_text)
