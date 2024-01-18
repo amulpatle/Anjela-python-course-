481949a9857723953ac577337664dc338f8dfe74
amul patle <amulpatle3@gmail.com>
2024-01-18 08:17:09 +0530

    check all condition

--- Day 33/main.py
+++ Day 33/main.py
@@ -1,35 +1,43 @@
 import requests
 from datetime import datetime
 MY_LAT = 21.812876
-MY_LOG = 80.183830
+MY_LOG = -80.183830
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
 
-# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    print(response.status_code)
 
-# print(response.status_code)
+    data = response.json()
+    iss_latitude = data["iss_position"]["latitude"]
+    iss_longitude = data["iss_position"]["longitude"]
+    iss_position = (iss_latitude,iss_longitude)
+    # print(data)
+    print(iss_position)
 
-# data = response.json()
-# longitude = data["iss_position"]["longitude"]
-# latitude = data["iss_position"]["latitude"]
-# iss_position = (longitude,latitude)
-# # print(data)
-# print(iss_position)
-
-parameter = {
-    "lat": MY_LAT,
-    "lng": MY_LOG,
-    "formatted":0
-}
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT and MY_LOG -5 <= iss_longitude <= MY_LOG+5:
+        return True
+def is_night():
+    parameter = {
+        "lat": MY_LAT,
+        "lng": MY_LOG,
+        "formatted":0
+    }
 
 
-resonponse = requests.get("https://api.sunrise-sunset.org/json", params=parameter)
-resonponse.raise_for_status()
-data = resonponse.json()
+    resonponse = requests.get("https://api.sunrise-sunset.org/json", params=parameter)
+    resonponse.raise_for_status()
+    data = resonponse.json()
 
-# print(data["results"]["sunrise"])
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
-print(sunrise)
-print(sunset)
+    # print(data["results"]["sunrise"])
+    sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+    sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
-time_now = datetime.now()
-print(time_now.hour)
+    time_now = datetime.now().hour
+    
+    if time_now >=sunset or time_now <=sunrise:
+        return True
+# print(time_now.hour)
+
+
+if is_iss_overhead() && is_night():
+    # THen send sms
