afd92c4d52b46964b390caf98853daea5d4f3bac
amul patle <amulpatle3@gmail.com>
2024-01-19 14:14:03 +0530

    check answer write or wronge

--- Day 34/Quiz app/ui.py
+++ Day 34/Quiz app/ui.py
@@ -15,19 +15,37 @@ class UserInterface:
         self.score_label.grid(row=0,column=1)
         
         self.canvas = Canvas(width=300,height=250,bg="white")
-        self.question_text = self.canvas.create_text(150,150,text="Some Question Text",fill=THEME_COLOR,font=("Arial",20,"italic"))
+        self.question_text = self.canvas.create_text(150,125,text="Some Question Text",fill=THEME_COLOR,font=("Arial",20,"italic"))
         self.canvas.grid(row = 1,column = 0, columnspan = 2,pady=50)
         
         true_image = PhotoImage(file="/home/amul/Documents/anjela python/Day 34/Quiz app/images/true.png")
-        self.true_button = Button(image=true_image,highlightthickness=0)
+        self.true_button = Button(image=true_image,highlightthickness=0,command=self.true_pressed)
         self.true_button.grid(row=2,column=0)
         
         false_imagee = PhotoImage(file="/home/amul/Documents/anjela python/Day 34/Quiz app/images/false.png")
-        self.false_button = Button(image=false_imagee,highlightthickness=0)
-        self.false_button.grid(row=2,column=1)
         
+        self.false_button = Button(image=false_imagee,highlightthickness=0,command=self.false_pressed)
+        self.false_button.grid(row=2,column=1)
+        self.get_next_question()
         self.window.mainloop()
     
     def get_next_question(self):
+        self.canvas.config(bg="white")
+        self.canvas.config(bg="white")
+        # if self.quiz.still_has_questions():
+        self.score_label.config(text=f"Score: {self.quiz.score}")
         q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text,text = q_text)
+        self.canvas.itemconfig(self.question_text, text=q_text)
+        # else:
+        # self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
+    def true_pressed(self):
+           self.give_feedback(self.quiz.check_answer("True"))
+        
+    def false_pressed(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+    def give_feedback(self,is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000,self.get_next_question)
