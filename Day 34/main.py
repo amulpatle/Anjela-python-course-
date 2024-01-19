392b746c16b4492166cf36c1c6926c1b7880e0a6
amul patle <amulpatle3@gmail.com>
2024-01-19 09:03:32 +0530

    type check

--- Day 34/main.py
+++ Day 34/main.py
@@ -7,14 +7,25 @@ parameters = {
     "type":"boolean",
 }
 
-response = requests.get(url="https://opentdb.com/api.php?",params=parameters)
-response.raise_for_status()
-data = response.json()
+# response = requests.get(url="https://opentdb.com/api.php?",params=parameters)
+# response.raise_for_status()
+# data = response.json()
 
-listdata = data["results"]
-# print(listdata[0])
-distdata = listdata[0]
-questiondata = distdata["question"]
-print(questiondata)
+# listdata = data["results"]
+# # print(listdata[0])
+# distdata = listdata[0]
+# questiondata = distdata["question"]
+# print(questiondata)
 
 # print(data["results"])
+
+
+def police_check(age:int)->bool: # THis is called type check in python '->' this indicate this function return type in fucntion calling 
+    if age > 18:
+        can_drive = True
+    else:
+        can_drive = False
+    return can_drive
+
+
+police_check(12)
